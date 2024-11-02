00fe2f109c17a8523e1c306825bdd9897023bb9b
Abdullahi Yusuf <abdullahiyusuf182@gmail.com>
2024-11-02 06:53:53 +0100

    Practiced a use case for the Async-Await feature The desktop app in dotNet was used to implement the async-await method.

---
+++ MoshAdvance_Async_Await/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MoshAdvance_Async_Await
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //DownloadHtmlAsync("http://msdn.microsoft.com");
+            
+            var getHtmlTask = GetHtmlAsync("http://msdn.microsoft.com");
+            MessageBox.Show("Waiting for task to complete");
+
+            var html = await getHtmlTask;
+            MessageBox.Show(html.Substring(1, 10));
+
+
+        }
+        public async Task<string> GetHtmlAsync(string url)
+        {
+            var webClient = new WebClient();
+
+            return await webClient.DownloadStringTaskAsync(url);
+        }
+        public string GetHtml(string url)
+        {
+            var webClient = new WebClient();
+
+            return webClient.DownloadString(url);
+        }
+        public async void DownloadHtmlAsync(string url)
+        {
+            var webClient = new WebClient();
+            var html = await webClient.DownloadStringTaskAsync(url);
+
+            using (StreamWriter streamWriter = new StreamWriter(@"c:\Admin\Users\Source.html"))
+            {
+                await streamWriter.WriteLineAsync(html);
+            }
+        }
+        public void DownloadHtml(string url)
+        {
+            var webClient = new WebClient();
+            var html = webClient.DownloadString(url);
+
+            using (StreamWriter streamWriter = new StreamWriter(@"c:\Admin\Users\Source.html"))
+            {
+                streamWriter.WriteLine(html);
+            }
+        }
+    }
+}
