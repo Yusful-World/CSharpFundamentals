5b6f1245e531dbb364bb35a8f19a823aabe27704
Abdullahi Yusuf <abdullahiyusuf182@gmail.com>
2024-11-04 14:08:08 +0100

    did some minor modifications

--- Bulky/BulkyWebRazor/obj/Debug/net8.0/BulkyWebRazor.AssemblyInfo.cs
+++ Bulky/BulkyWebRazor/obj/Debug/net8.0/BulkyWebRazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWebRazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87fb02889b972fe209933b1876bbd31321bc629f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c4aea66cbe3c938b16dd842211fc34f19dd172e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWebRazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWebRazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
