3f493aa304fddccf2dda082e04abf064f8e41504
Abdullahi Yusuf <abdullahiyusuf182@gmail.com>
2024-11-11 17:22:09 +0100

    created razor pages for category model and implemented crud operations

--- Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b6f1245e531dbb364bb35a8f19a823aabe27704")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+263e38f4cfd468d596ae721392cf4f94d557f55e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
